c1f49f8feda95b72e8d85615fa795095aadc2901
zhugongpu <zhugongpu@hotmail.com>
2015-02-02 20:51:38 +0800

    添加Comment Model

---
+++ app/src/main/java/com/bainiaohe/dodo/main/fragments/info/model/CommentModel.java
@@ -0,0 +1,12 @@
+package com.bainiaohe.dodo.main.fragments.info.model;
+
+/**
+ * Created by zhugongpu on 15/2/2.
+ */
+public class CommentModel {
+    public String id = null;
+    public String avatar = null;
+    public String name = null;
+    public String time = null;
+    public String content = null;
+}
