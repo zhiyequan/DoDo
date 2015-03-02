d356543a8d9a70d5302ef6ea7d279ce462893386
lewisKit <lewsifyh2012@gmail.com>
2015-03-02 17:35:35 +0800

    新增发送短信和获取手机联系人方法

--- app/src/main/java/com/bainiaohe/dodo/DoDoContext.java
+++ app/src/main/java/com/bainiaohe/dodo/DoDoContext.java
@@ -1,8 +1,14 @@
 package com.bainiaohe.dodo;
 
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.os.Build;
 import android.preference.PreferenceManager;
+import android.provider.ContactsContract;
+import android.telephony.SmsManager;
 
 import com.bainiaohe.dodo.model.User;
 import com.sea_monster.core.utils.Base64;
@@ -12,6 +18,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import io.rong.imkit.RongIM;
@@ -22,6 +29,18 @@ import io.rong.imlib.RongIMClient.UserInfo;
  */
 public class DoDoContext {
 
+    //查询手机联系人的返回值
+    @SuppressLint("InlineApi")
+    private static final String[] PROJECTION =
+            {
+                    ContactsContract.Contacts._ID,
+                    Build.VERSION.SDK_INT
+                            >= Build.VERSION_CODES.HONEYCOMB ?
+                            ContactsContract.Contacts.DISPLAY_NAME_PRIMARY :
+                            ContactsContract.Contacts.DISPLAY_NAME
+            };
+    private static final int CONTACT_ID_INDEX = 0;
+    private static final int CONTACT_NAME_INDEX = 1;
     private static DoDoContext instance;
     private Context mContext;
     private SharedPreferences mSharedPreferences;
@@ -167,5 +186,78 @@ public class DoDoContext {
         return user;
     }
 
+    /**
+     * 发送短信，这里采用的是如果超过字数限制则发送多条短信
+     */
+    public void sendMessage(String message, String phoneNumber) {
+
+        SmsManager sms = SmsManager.getDefault();
+
+        message = message + " 欢迎使用职圈科技/www.baidu.com";
+        ArrayList<String> messages = sms.divideMessage(message);
+
+        sms.sendMultipartTextMessage(phoneNumber, null, messages, null, null);
+
+    }
+
+    /**
+     * 获得手机所有联系人 信息包括名字和电话号码
+     */
+    public ArrayList<HashMap<String, String>> getPhoneContacts() {
+
+        ArrayList<HashMap<String, String>> phoneContacts = new ArrayList<>();
+
+        ContentResolver resolver = mContext.getContentResolver();
+
+        //从数据库中得到手机联系人 查询条件:含有电话号码
+        Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, PROJECTION,
+                ContactsContract.Contacts.HAS_PHONE_NUMBER + " = 1", null, null);
+
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                String id = cursor.getString(CONTACT_ID_INDEX);
+                String name = cursor.getString(CONTACT_NAME_INDEX);
+
+                //查询每一个的手机号码
+                Cursor pCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                        new String[]{id}, null);
+
+                while (pCursor.moveToNext()) {
+                    HashMap<String, String> contact = new HashMap<>();
+                    String phonNumber = getFormatePhoneNumber(pCursor.getString(0));
+                    if (phonNumber != null) {
+                        contact.put("contactName", name);
+                        contact.put("number", phonNumber);
+
+                        phoneContacts.add(contact);
+                    }
+
+                }
+                pCursor.close();
+            }
+        }
+
+
+        return phoneContacts;
+    }
+
+    /**
+     * 格式化手机号码 暂时只考虑中国的
+     *
+     * @param number
+     * @return
+     */
+    private String getFormatePhoneNumber(String number) {
+        String res = number.replace('-', ' ').replaceAll("\\s", "");
+        if (res.length() >= 11) {
+
+            res = res.substring(res.length() - 11);
+            return res;
+
+        }
+        return null;
+    }
 
 }
